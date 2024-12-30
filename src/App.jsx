0c7313d45357fd9ad254dd60e39aa25d28e47c7a
Kevin Smith <mrkevinsmithdesign@yahoo.com>
2024-12-30 10:25:29 -0600

    Setup vite app and added MUI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </>
+  );
+}
+
+export default App;
